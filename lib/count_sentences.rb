225dba8f216d6f50dbcdcd54be93910bf5b0628c
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-08-22 14:27:34 +0300

    commit tree c4eaa4f5142eda8d6aa47ff2d9a018561e48e6d2

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".")  
+    # puts self
   end
-
+ 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+     self.end_with?("!")
 
   end
 
   def count_sentences
-
-  end
+     sentence= self.split(/[.!?]/)
+     count= sentence.filter {|sent1| !sent1.empty?}
+      count.length
+    end
 end
+
+word = String.new
+"why are you running.".sentence?
+"why are you eating.".question?
+"why are you sitting!".exclamation?
+"why are you watchimg.why are you running.why are you sitting!".count_sentences
